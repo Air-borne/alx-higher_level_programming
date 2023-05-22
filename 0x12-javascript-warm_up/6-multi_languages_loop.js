49ffdda04a6a4a70a898b1b603989e1410e31e3c
Bernard Sakyi <sakyibernard77@gmail.com>
2023-05-22 22:47:55 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringArrays = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; ++i) {
-  console.log(languages[i]);
-}
+stringArrays.forEach(element => {
+  console.log(element);
+});
