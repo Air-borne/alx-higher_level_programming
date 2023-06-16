0f2e463cc52b20679932dbfda35d27d28a5402e1
bernardsakyi <sakyibernard77@gmail.com>
2023-06-16 09:50:48 +0000

    correct code

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" -X OPTIONS | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
+# Display allowed methods on the given url
+curl -sI $1 | grep Allow | cut -c8-
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable 'email' must be sent with the value 'hr@holbertonschool.com'. A variable 'subject' must be sent with the value 'I will always be here for PLD'.
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# Sends a post request to given url
+curl -sd 'email=test@gmail.com&subject=I will always be here for PLD' -X POST $1
